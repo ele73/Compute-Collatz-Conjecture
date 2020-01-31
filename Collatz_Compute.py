b898740df498e09be50a53abc048b0799896cc3e
ele73 <60517066+ele73@users.noreply.github.com>
2020-01-31 18:25:40 +0100

    Updated and renamed 3n+1.py to Collatz_Compute.py
    Added a description to each function corrected a problem in the first for where it returned the wrong number of steps.

--- Collatz_Compute.py
+++ Collatz_Compute.py
@@ -1,5 +1,11 @@
+"""
+This algorithm takes a number n. If n is even, n is multiplied by 0.5. If n is odd, it's multiplied by 3 and it adds one 
+to the result.
+"""
+
 def show_collatz(n):
-    res = int(n)
+    """This functions the steps n takes to get to 1 an what the steps are"""
+    res = int(n) # The result of the computations
     print("Step 0: " + str(int(res)))
     for i in range(1, 10000):
         if res % 2 == 0:
@@ -7,8 +13,8 @@ def show_collatz(n):
             print("Step " + str(i) + ": " + str(int(res)))
         elif res == 1:
             print("n = " + str(n))
-            return True
-            n_steps = i+1
+            n_steps = i-1
+            return True # It means the Collatz Conjecture holds true in this case
             return n_steps
             break
         else:
@@ -16,6 +22,9 @@ def show_collatz(n):
             print("Step " + str(i) + ": " + str(int(res)))
 
 def collatz_true(n):
+    """This function takes in a number and shows all the numbers beetween 
+    1 and n and shows how many steps each number takes to get to 1
+    """
     global c1
     c1 = False
     for e in range(n):
@@ -32,10 +41,11 @@ def collatz_true(n):
         if c1 == True:
             print(str(e+1) + ": " + str(n_steps) + " steps.") 
 
-def collatz_true1(n):
+def collatz_true_alt(n, a):
+    """This function does the same as collatz_true but this time from n to n+a"""
     global c1
     c1 = False
-    for e in range(n, n+10000):
+    for e in range(n, n+a):
         res = e+1
         for i in range(100000):
             if res % 2 == 0:
